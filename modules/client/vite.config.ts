0fc64b374233a7f796a50f1f76c5ed689ed95a07
lorefnon <lorefnon@gmail.com>
2023-03-19 23:37:59 +0530

    Configure vite to populate build assets in server's public directory

--- modules/client/vite.config.ts
+++ modules/client/vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
+import path from 'node:path'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: path.join(__dirname, '../server/public')
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:8083',
